701e34d7d11d24f521cd6c415ad4f3a3840c9f8f
Dominic Sudy <dominic.sudy@bosch.com>
2023-02-27 07:04:27 +0100

    Disable kubernetes installation due to 500 internal error

--- common/install-k3d-prerequisites.sh
+++ common/install-k3d-prerequisites.sh
@@ -24,10 +24,10 @@ chmod 700 get_helm.sh
 sudo chmod +x /usr/local/bin/helm
 
 # Install kubectl
-if ! command -v kubectl &> /dev/null
-then
-    sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-    sudo apt-get update
-    sudo apt-get install -y kubectl
-fi
+#if ! command -v kubectl &> /dev/null
+#then
+#    sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+#    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#    sudo apt-get update
+#    sudo apt-get install -y kubectl
+#fi
