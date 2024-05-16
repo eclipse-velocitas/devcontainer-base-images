d250fd644f8664f6c205c287f85cdfd68740ba10
dennismeister93 <dennis.meister@bosch.com>
2024-05-16 12:28:21 +0200

    Update minor velocitas cli version (#66)
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- common/install-velocitas-cli.sh
+++ common/install-velocitas-cli.sh
@@ -13,7 +13,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 set -e
-CLI_INSTALL_VERSION='v0.9.0' # reference a particular released version
+CLI_INSTALL_VERSION='v0.9.1' # reference a particular released version
 echo "#######################################################"
 echo "### Install Velocitas CLI ${CLI_INSTALL_VERSION}                    ###"
 echo "#######################################################"
